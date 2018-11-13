ce7683f3937ca6fd5f1e10313d9c164b4a3d082c
Nwachinemere Ibeagi <fuzzy245.in@gmail.com>
2018-11-13 11:25:14 +0100

    Initial commit

---
+++ StoreManager/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using StoreManager.Models;
+
+[assembly: OwinStartupAttribute(typeof(StoreManager.Startup))]
+namespace StoreManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRole();
+        }
+
+        private void CreateRole()
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_context));
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Staff"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Staff";
+                roleManager.Create(role);
+            }
+        }
+    }
+}
